ab3c4d5ddd7f5e934419c4cb80452f5394a91297
unknown <frankmnganga@gmail.com>
2020-12-11 10:56:28 +0300

    fibonacci iterative solution

--- fib/index.js
+++ fib/index.js
@@ -8,6 +8,30 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
 
+    let a = 0;
+    let b = 1;
+
+    let nth = 1;
+
+    for(let i = 2; i <= n; i++){
+        let next = a + b;
+        a = b;
+        b = next;
+        nth = next;
+    }
+    return nth;
+}
+
+console.log(fib(39));
+
+
+const cache = {};
+const fib2 = (n) => {
+    if(n < 2) return cache[n];
+
+    cache[n] = fib2(n - 1) + fib2(n - 2);
+
+}
 module.exports = fib;
