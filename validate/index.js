66a2891b9f0effe2bceebb8c9a677fce87d587f3
unknown <frankmnganga@gmail.com>
2020-12-28 18:06:08 +0300

    minor fix, missing return

--- validate/index.js
+++ validate/index.js
@@ -5,6 +5,11 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+	if (node.left.data > max) return false;
+	if (node.right.data < min) return false;
+
+	// validate(node.left);
+}
 
 module.exports = validate;
