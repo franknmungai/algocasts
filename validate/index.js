066292d45992c597df6c3edc302ba0afb1f827fc
unknown <frankmnganga@gmail.com>
2020-12-30 18:30:22 +0300

    validate bst 1

--- validate/index.js
+++ validate/index.js
@@ -9,7 +9,12 @@ function validate(node, min = null, max = null) {
 	if (node.left.data > max) return false;
 	if (node.right.data < min) return false;
 
-	// validate(node.left);
+	return (
+		node.left &&
+		validate(node.left, node.data, node.data) &&
+		node.right &&
+		validate(node.right, node.data, node.data)
+	);
 }
 
 module.exports = validate;
