3a7cbabc55672c88b28d4e17fd45c0a557d9dbdc
unknown <frankmnganga@gmail.com>
2021-01-15 21:35:43 +0300

    bubble sort

--- sorting/index.js
+++ sorting/index.js
@@ -2,19 +2,23 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr.length - i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				const temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	return arr;
 }
 
-function selectionSort(arr) {
+function selectionSort(arr) {}
 
-}
+function mergeSort(arr) {}
 
-function mergeSort(arr) {
+function merge(left, right) {}
 
-}
-
-function merge(left, right) {
-
-}
-
+console.log(bubbleSort([100, -40, 500, -124, 0, 21, 7]));
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
